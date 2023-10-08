8fa907531bba40cd1cc0391d40d84d2e52677930
= <=>
2023-10-08 14:49:58 +0100

    first commit

---
+++ loops.js
@@ -0,0 +1,17 @@
+
+
+// write a for loop that prints your name 10 times without using code snpippets or any other refence
+ 
+for (let i=0; i<20; i++) {
+    console.log("Ahmed")
+}
+
+// write a while loop that prints your surname 20 times
+
+let i = 0;
+
+while (i < 20) {
+    console.log("Dini")
+    i++;
+}
+
